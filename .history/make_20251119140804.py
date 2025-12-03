68c1fc3d7f59768edc2d80fdfd2460de0d4ec3fb
PieterJansma <jansma.pieter@outlook.com>
2025-12-03 13:02:18 +0100

    Remove .venv and .history from repo

--- .history/make_20251119140804.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_excel('/Users/p.jansma/Documents/GitHub/paper-extracting/UMCG_Resources1763456291774.xlsx')[0]
-
-print(df)
--- .history/make_20251119140821.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_excel('/Users/p.jansma/Documents/GitHub/paper-extracting/UMCG_Resources1763456291774.xlsx')
-
-print(df)
